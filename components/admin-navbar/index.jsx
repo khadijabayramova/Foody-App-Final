b7a3f4bbebc54ef5e38c8652300adda8d9c18a69
perinov <novruzovaperi2@gmail.com>
2024-09-20 16:16:27 +0400

    create nav

---
+++ components/admin-navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex items-center justify-center ">
+        <div className="bg-slate-500 w-[1400px] h-16 p-2 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          debitis officiis, nobis nihil repellendus unde. Quae optio deserunt
+          cupiditate amet ipsum facere atque esse, qui possimus fugit minus
+          consectetur veniam
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import config from "../configs/config.js";
-
-const authData = require("../configs/config.js");
-
-console.log("Auth Data from .env:", authData);
-
-console.log("Auth Key:", authData.type);
+import Navbar from "../components/admin-navbar";
 
 export default function index() {
   return (
     <div>
-      <h2>parii</h2>
+      <Navbar />
     </div>
   );
 }
