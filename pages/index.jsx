f7aec17d3139ca7bf1437e193df8304c08f75df5
khadijabayramova <safarlikhadija98@gmail.com>
2024-09-21 00:34:37 +0400

    updating data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/admin-navbar";
+import Login from "./admin/login";
 
 export default function index() {
   return (
     <div>
       <Navbar />
+      <Login />
     </div>
   );
 }
