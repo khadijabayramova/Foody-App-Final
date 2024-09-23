424142cb4f023ee979c2329e45672aee191cc649
khadijabayramova <safarlikhadija98@gmail.com>
2024-09-23 14:31:20 +0400

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "../components/admin-navbar";
 
-
 export default function index() {
   return (
     <>
       <Navbar />
-     
-    </div>
+    </>
   );
 }
