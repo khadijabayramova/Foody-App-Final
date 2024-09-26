2ad4837412e35eea369eaeaf69f4230fbef989fe
perinov <novruzovaperi2@gmail.com>
2024-09-26 18:09:43 +0400

    editing btns

--- components/pages/restaurants/RestaurantCard/RestarantCard.jsx
+++ components/pages/restaurants/RestaurantCard/RestarantCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function RestarantCard({ name, category }) {
   return (
-    <div className="h-20 bg-white rounded-md flex items-center p-3 gap-4 text-black">
+    <div className="h-20 bg-white rounded-md flex items-center justify-between p-4 text-black">
       <div>
         <Image
           src="/adminImgs/papajons.svg"
@@ -15,6 +15,24 @@ export default function RestarantCard({ name, category }) {
       <div>
         <h2 className="font-semibold text-lg ">{name}</h2>
         <p className="text-restautanColor">{category}</p>
+      </div>
+      <div className="p-4 flex gap-2 flex-col">
+        <button>
+          <Image
+            src="/adminImgs/editing/edit.svg"
+            alt="edit"
+            width={20}
+            height={20}
+          />
+        </button>
+        <button>
+          <Image
+            src="/adminImgs/editing/delete.svg"
+            alt="edit"
+            width={20}
+            height={20}
+          />
+        </button>
       </div>
     </div>
   );
