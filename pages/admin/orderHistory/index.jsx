9941b6a8c6374395269b812c9bd87063795bd778
perinov <novruzovaperi2@gmail.com>
2024-10-03 19:28:15 +0400

    created table component

--- pages/admin/orderHistory/index.jsx
+++ pages/admin/orderHistory/index.jsx
@@ -1,43 +1,78 @@
 import React from "react";
 import AdminLayout from "../../../components/layout/AdminLayout/AdminLayout";
-import OrderHistoryTable from "../../../components/pages/orders/OrderHistory/OrderHistoryTable";
 import Button from "../../../components/shared/Button/page";
+import TableComponent from "../../../components/pages/tablecomp/TableComponent";
 
 export default function OrderHistoryPage() {
-  const data = new Array(16).fill(1);
+  const headers = [
+    "ID",
+    "Customer ID",
+    "Time",
+    "Delivery Address",
+    "Amount",
+    "Payment Methods",
+    "Contact",
+  ];
+
+  console.log(headers, "heaaaaad");
+
+  const data = [
+    {
+      id: 1,
+      customerId: 4545,
+      time: "10:00",
+      deliveryAddress: "30 Eve Street, 543 Evenue Road, Ny 87876",
+      amount: 17.99,
+      paymentMethods: "Cash on delivery",
+      contact: 9944445566,
+    },
+    {
+      id: 5,
+      customerId: 4545,
+      time: "10:00",
+      deliveryAddress: "30 Eve Street, 543 Evenue Road, Ny 87876",
+      amount: 17.99,
+      paymentMethods: "Cash on delivery",
+
+      contact: 9944445566,
+    },
+    {
+      id: 4,
+      customerId: 4545,
+      time: "10:00",
+      deliveryAddress: "30 Eve Street, 543 Evenue Road, Ny 87876",
+      amount: 17.99,
+      paymentMethods: "Cash on delivery",
+      contact: 9944445566,
+    },
+  ];
 
   return (
     <AdminLayout>
       <div className="w-[82%]">
         <Button name="History" />
 
-        <div className="overflow-y-auto h-[61vh] mt-4 ">
-          <table class="min-w-full bg-white text-black border   ">
-            <thead>
-              <tr>
-                <th class="px-4 py-4 text-start">ID</th>
-                <th class="px-4 py-4 text-start"> Customer ID</th>
-                <th class="px-4 py-4 text-start">Time</th>
-                <th class="px-4 py-4 text-start">Delivery Address</th>
-                <th class="px-4 py-4 text-start">Amount</th>
-                <th class="px-4 py-4 text-start">Payment Method</th>
-                <th class="px-4 py-4 text-start">Contact</th>
-              </tr>
-            </thead>
-
-            {data.map(() => (
-              <OrderHistoryTable
-                id="12122"
-                customerId="12122"
-                time="12:00"
-                deliveryAddress="123 Main St, Anytown USA"
-                amount="2000"
-                paymentMethods="Cash"
-                contact="1234567890"
-              />
-            ))}
-          </table>
-        </div>
+        <TableComponent headers={headers}>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td className="px-8 py-4 border-y ">
+                <span className="border border-#DFE2E9 px-4 py-1 rounded-3xl">
+                  {row.id}
+                </span>
+              </td>
+              <td className="px-8 py-4 border-y ">
+                <span className="border border-#DFE2E9 px-4 py-1 rounded-3xl">
+                  {row.customerId}
+                </span>
+              </td>
+              <td className="px-8 py-4 border-y ">{row.time}</td>
+              <td className="px-8 py-4 border-y ">{row.deliveryAddress}</td>
+              <td className="px-8 py-4 border-y ">{row.amount}</td>
+              <td className="px-8 py-4 border-y ">{row.paymentMethods}</td>
+              <td className="px-8 py-4 border-y ">{row.contact}</td>
+            </tr>
+          ))}
+        </TableComponent>
       </div>
     </AdminLayout>
   );
