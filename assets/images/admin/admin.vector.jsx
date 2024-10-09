153f86cfb5306cc9d588fb505247202be7fd0f80
Sedi98 <sedi9816@gmail.com>
2024-10-10 00:38:40 +0400

    fixed orders and some updates

--- assets/images/admin/admin.vector.jsx
+++ assets/images/admin/admin.vector.jsx
@@ -8,8 +8,8 @@ export const AddIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M8.20332 4.08984V8.31353H4.13184V9.72142H8.20332V13.9451H9.56047V9.72142H13.632V8.31353H9.56047V4.08984H8.20332Z"
         fill="white"
       />
@@ -29,6 +29,24 @@ export const BurgerIcon = () => {
       <path
         d="M16.25 18H3.25V16H16.25V18ZM22.75 6V8H3.25V6H22.75ZM16.25 13H3.25V11H16.25V13Z"
         fill="#F5F5F5"
+      />
+    </svg>
+  );
+};
+
+export const TrashIcon = () => {
+  return (
+    <svg
+      width="14"
+      height="17"
+      viewBox="0 0 14 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11 5.66667V15.1111H3V5.66667H11ZM9.5 0H4.5L3.5 0.944444H0V2.83333H14V0.944444H10.5L9.5 0ZM13 3.77778H1V15.1111C1 16.15 1.9 17 3 17H11C12.1 17 13 16.15 13 15.1111V3.77778Z"
+        fill="#EB5757"
+        fill-opacity="0.61"
       />
     </svg>
   );
--- pages/admin/orders/index.jsx
+++ pages/admin/orders/index.jsx
@@ -1,14 +1,53 @@
-import React from "react";
-import TableComponent from "../../../components/pages/tablecomp/TableComponent";
+import React, { useState, useEffect } from "react";
+import TableComponent from "../../../components/pages/TableComponent/TableComponent";
 import Button from "../../../components/shared/Button/page";
 import AdminLayout from "../../../components/layout/AdminLayout/AdminLayout";
+import TableRow from "../../../components/pages/tablecomp/TableRow/TableRow";
+import { getAllOrders } from "../../../services/axios";
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     let resp = await getAllOrders();
+  //     if (resp) {
+  //       console.log(resp);
+  //     }
+  //   })();
+  // }, []);
+
   return (
     <AdminLayout>
       <div className="w-full">
         <Button name="Orders" />
 
-        <TableComponent />
+        <TableComponent
+          headers={[
+            "ID",
+            "Customer ID",
+            "Time",
+            "Delivery Address",
+            "Payment Method",
+            "Amount",
+            "Contact",
+            "",
+          ]}
+        >
+          <TableRow
+            data={{
+              id: 1,
+              customer_id: 4545,
+              created: "10:00",
+              delivery_address: "30 Eve Street, 543 Evenue Road, Ny 87876",
+              amount: 17.99,
+              payment_method: "Cash on delivery",
+              contact: "+9944445566",
+            }}
+            handleDelete={() => {
+              console.log("delete");
+            }}
+          />
+        </TableComponent>
       </div>
     </AdminLayout>
   );
